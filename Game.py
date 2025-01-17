8c6fea25a2c8473be615bd52da5d4eea6d963c9d
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2025-01-17 22:13:56 +0100

    Creation des objets de bases et début de code

---
+++ Game.py
@@ -0,0 +1,6 @@
+import pygame
+
+pygame.init()
+
+font = pygame.font.Font("freesansbold.ttf", 16)
+timer = pygame.time.Clock()
---
+++ Player.py
@@ -0,0 +1,5 @@
+import pygame
+
+class Player:
+    def __init__(self):
+        self.sprite = pygame.image.load("doodle.png")
