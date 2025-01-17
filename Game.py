543afe3ea283aab3cca8650575b2265c63debf6e
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2025-01-17 22:43:07 +0100

    Affichage ok

--- Game.py
+++ Game.py
@@ -1,6 +1,27 @@
 import pygame
+from Player import *
 
 pygame.init()
 
 font = pygame.font.Font("freesansbold.ttf", 16)
 timer = pygame.time.Clock()
+backround = (255,255,255)
+
+screen = pygame.display.set_mode([400, 500]) #setup de la taille de l'écran
+pygame.display.set_caption("Doodle qui Jump") #nom de la fenêtre de jeu
+player = Player()
+
+running = True
+while running:
+    timer.tick(60) #setup des fps
+    screen.fill(backround)
+    screen.blit(player.sprite, (player.x, player.y))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+
+
+pygame.quit()
--- Player.py
+++ Player.py
@@ -2,4 +2,6 @@ import pygame
 
 class Player:
     def __init__(self):
-        self.sprite = pygame.image.load("doodle.png")
+        self.sprite = pygame.transform.scale(pygame.image.load("doodle.png"), (70,70))
+        self.x = 170
+        self.y = 400
