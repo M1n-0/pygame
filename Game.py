b333757da527065055f14afa9b1eaee18f40fad7
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2025-01-18 14:54:01 +0100

    First plateform

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import pygame
 from Player import *
+from Plateform import *
 
 pygame.init()
 
@@ -10,12 +11,18 @@ backround = (255,255,255)
 screen = pygame.display.set_mode([400, 500]) #setup de la taille de l'écran
 pygame.display.set_caption("Doodle qui Jump") #nom de la fenêtre de jeu
 player = Player()
+plateforms = [Plateform()]
 
 running = True
 while running:
     timer.tick(60) #setup des fps
     screen.fill(backround)
     screen.blit(player.sprite, (player.x, player.y))
+    blocks = []
+
+    for i in range(len(plateforms)):
+        block = pygame.draw.rect(screen, (0, 0, 0), plateforms[i].co, plateforms[i].type, 4)
+        blocks.append(block)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- Plateform.py
+++ Plateform.py
@@ -1,1 +1,6 @@
 import pygame
+
+class Plateform:
+    def __init__(self, co = [165, 480, 70, 10], type = 0):
+        self.co = co
+        self.type = type
