0cf9f19a9de290731607f742dd8ae975e3fbfa3d
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2025-01-18 21:55:10 +0100

    Encore et toujours des ajustements de spawn, et le score (basique sans save)

--- Game.py
+++ Game.py
@@ -8,11 +8,14 @@ pygame.init()
 font = pygame.font.Font("freesansbold.ttf", 16)
 timer = pygame.time.Clock()
 backround = (255,255,255)
+score = 0
+hightScore = 0
+gameOver = False
 
 screen = pygame.display.set_mode([400, 500]) #setup de la taille de l'écran
 pygame.display.set_caption("Doodle qui Jump") #nom de la fenêtre de jeu
 player = Player()
-plateforms = [Plateform(), Plateform(randint(0, 330), randint(300, 400), 0), Plateform(randint(0, 330), randint(220, 300), 0), Plateform(randint(0, 330), randint(120, 220), 0)]
+plateforms = [Plateform(), Plateform(randint(0, 330), randint(320, 420), 0), Plateform(randint(0, 330), randint(270, 370), 0), Plateform(randint(0, 330), randint(240, 320), 0),Plateform(randint(0, 330), randint(170, 270), 0)]
 
 
 running = True
@@ -20,6 +23,8 @@ while running:
     timer.tick(60) #setup des fps
     screen.fill(backround)
     screen.blit(player.sprite, (player.x, player.y))
+    scoreText = font.render("Score: " + str(score), True, (120,120,120), backround)
+    screen.blit(scoreText, (320, 20))
     blocks = []
 
     for i in range(len(plateforms)):
@@ -49,8 +54,9 @@ while running:
     if player.y < 200 and player.yChange < 0:
         for i in range(len(plateforms)):
             plateforms[i].affichage(player.yChange)
-            if plateforms[i].affichage(player.yChange): #ici on remplace la plateforme si elle sort par une nouvelle en haut de l'écran
-                plateforms[i] = Plateform(randint(0, 330), randint(-50, -10), 0)
+            if plateforms[i].affichage(player.yChange): #ici on remplace la plateforme si elle sort par une nouvelle en haut de l'écran et on ajout +1 au score
+                plateforms[i] = Plateform(randint(0, 330), randint(-30, 10), 0)
+                score += 1
 
     pygame.display.flip()
 
