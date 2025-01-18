fbafeb18c1536fae84faf3e570cfb62b9e91de3f
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2025-01-18 16:33:30 +0100

    Génération des plateformesde start  aléatoires

--- Game.py
+++ Game.py
@@ -12,7 +12,7 @@ backround = (255,255,255)
 screen = pygame.display.set_mode([400, 500]) #setup de la taille de l'écran
 pygame.display.set_caption("Doodle qui Jump") #nom de la fenêtre de jeu
 player = Player()
-plateforms = [Plateform(), Plateform(randint(0, 330), randint(250, 450), 0), Plateform(randint(0, 330), randint(250, 450), 0)]
+plateforms = [Plateform(), Plateform(randint(0, 330), randint(250, 450), 0), Plateform(randint(0, 330), randint(250, 450), 0), Plateform(randint(0, 330), randint(100, 300), 0), Plateform(randint(0, 330), randint(100, 300), 0), Plateform(randint(0, 330), randint(20, 150), 0)]
 
 
 running = True
